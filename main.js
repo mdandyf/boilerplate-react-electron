a562bd7f4b5df2ad1bb12de4cd9eb7c94c13fce0
Mochamad Firmansyah <Mochamad.Firmansyah@petrosea.com>
2021-04-16 14:18:25 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
